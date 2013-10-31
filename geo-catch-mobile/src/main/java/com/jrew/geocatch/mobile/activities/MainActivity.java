170d3baf306acedc6d1a59a1fd5869690f83b761
jrew <it.jrew@gmail.com>
2013-10-31 09:44:12 +0300

    ActionBarSherlock is added to mobile project

--- geo-catch-mobile/src/main/java/com/jrew/geocatch/mobile/activities/MainActivity.java
+++ geo-catch-mobile/src/main/java/com/jrew/geocatch/mobile/activities/MainActivity.java
@@ -1,8 +1,12 @@
 package com.jrew.geocatch.mobile.activities;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.TextView;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 import com.jrew.geocatch.mobile.R;
 
 /**
@@ -12,15 +16,80 @@ import com.jrew.geocatch.mobile.R;
  * Time: 19:02
  * To change this template use File | Settings | File Templates.
  */
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends SherlockActivity implements ActionBar.TabListener {
+
+    private static int theme = R.style.Theme_Sherlock_Light;
+
+    public static int THEME = R.style.Theme_Sherlock_Light_DarkActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(theme); //Used for theme switching in samples
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.main_layout);
 
-        //Fragment mapFragment = getSupportFragmentManager().findFragmentById(R.id.mapFragment);
-        //Fragment bottomBarFragment = getSupportFragmentManager().findFragmentById(R.id.bottomBarFragment);
+        ActionBar actionBar = getSupportActionBar();
+
+        //The following two options trigger the collapsing of the main action bar view.
+        //See the parent activity for the rest of the implementation
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        actionBar.setHomeButtonEnabled(false);
+
+
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+
+//        for (int i = 1; i <= 3; i++) {
+//            ActionBar.Tab tab = actionBar.newTab();
+//            tab.setText("Tab " + i);
+//            tab.setIcon(R.drawable.ic_action_location);
+//            tab.setTabListener(this);
+//            actionBar.addTab(tab);
+//        }
+    }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getSupportMenuInflater().inflate(R.menu.menu_main, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+
+//        boolean isLight = THEME == R.style.Theme_Sherlock_Light;
+//
+//        menu.add("Save")
+//                .setIcon(isLight ? R.drawable.ic_compose_inverse : R.drawable.ic_compose)
+//                .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+//
+//        menu.add("Search")
+//                .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+//
+//        menu.add("Refresh")
+//                .setIcon(isLight ? R.drawable.ic_refresh_inverse : R.drawable.ic_refresh)
+//                .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+//
+//        return true;
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
--- geo-catch-mobile/src/main/java/com/jrew/geocatch/mobile/fragments/MapFragment.java
+++ geo-catch-mobile/src/main/java/com/jrew/geocatch/mobile/fragments/MapFragment.java
@@ -1,11 +1,14 @@
 package com.jrew.geocatch.mobile.fragments;
 
-import android.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+import com.actionbarsherlock.app.SherlockFragment;
 import com.jrew.geocatch.mobile.R;
 
 /**
@@ -16,19 +19,30 @@ import com.jrew.geocatch.mobile.R;
  * To change this template use File | Settings | File Templates.
  *
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends SherlockFragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        // Create fragment view
-        View mapFragmentView = inflater.inflate(R.layout.map_fragment, container, false);
-
-        // Set start up page to web view
-        WebView webView = (WebView) mapFragmentView.findViewById(R.id.mapView);
-        webView.loadUrl(getResources().getString(R.string.geoCatchRepositoryUrl));
-
-        return mapFragmentView;
-    }
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//
+//        // Create fragment view
+//        View mapFragmentView = inflater.inflate(R.layout.map_fragment, container, false);
+//
+//
+//        WebView webView = (WebView) mapFragmentView.findViewById(R.id.mapView);
+//        // enable javascript
+//        webView.getSettings().setJavaScriptEnabled(true);
+//
+//        final Activity activity = getActivity();
+//        webView.setWebViewClient(new WebViewClient() {
+//            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+//                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        // Set start up page
+//        webView.loadUrl(getResources().getString(R.config.geoCatchRepositoryUrl));
+//
+//        return mapFragmentView;
+//    }
 }
