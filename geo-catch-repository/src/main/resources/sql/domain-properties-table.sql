95587a7b282fe0e628b1dc63b4985b8f4cf1c794
unknown <Kazak_VV@KAZAK-VV.itd.iba.by>
2013-11-26 20:03:28 +0300

    Added domain property initial data sql scripts.

--- geo-catch-repository/src/main/resources/sql/domain-properties-table.sql
+++ geo-catch-repository/src/main/resources/sql/domain-properties-table.sql
@@ -1,11 +1,18 @@
-CREATE TABLE IF NOT EXISTS `trophy-map`.`domain_property` (
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `isDefault` TINYINT(1) NOT NULL,
-  `item` BIGINT(20) NOT NULL,
-  `locale` VARCHAR(255) NULL DEFAULT NULL,
-  `type` BIGINT(20) NOT NULL,
-  `value` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
+SET NAMES utf8;
+
+CREATE TABLE IF NOT EXISTS domain_property (
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  isDefault TINYINT(1) NOT NULL,
+  item BIGINT(20) NOT NULL,
+  locale VARCHAR(255) NULL DEFAULT NULL,
+  type BIGINT(20) NOT NULL,
+  value VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (id))
 ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8
+AUTO_INCREMENT = 3;
+
+-- In case if table were created by JPA need to update value column encoding
+ALTER TABLE domain_property CONVERT TO CHARACTER SET utf8
+COLLATE utf8_general_ci;
+
+commit;
